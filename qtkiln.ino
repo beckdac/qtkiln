78e6c2770ab1aa5c866b325bbeb475ccfb741d28
David Beck <beck.dac@live.com>
2025-03-17 21:04:25 -0700

    fixing up the settings for all json

--- qtkiln.ino
+++ qtkiln.ino
@@ -10,6 +10,7 @@
 #include <TM1637Display.h>
 #include <ArduinoJson.h>
 
+#include "qtkiln.h"
 #include "qtkiln_thermo.h"
 
 // thermocouple phy
@@ -23,34 +24,11 @@ QTKilnThermo *kiln_thermo = NULL;
 QTKilnThermo *housing_thermo = NULL;
 
 // WiFi credentials
-#define WIFI_SETUP_DELAY_MS 250
 #include "wifi_cred.h"
 const char *ssid = WIFI_SSID;
 const char *sspw = WIFI_PASS;
 
 // MQTT server
-#define MQTT_MAX_TOPIC_STR 256
-#define MQTT_TOPIC_BASE "qtkiln"
-#define MQTT_TOPIC_FMT "%s/%s"
-#define MQTT_TOPIC_KILN_FMT "%s/kiln"
-#define MQTT_KILN_TEMP_FMT "%lu %0.2f"
-#define MQTT_TOPIC_HOUSING_FMT "%s/housing"
-#define MQTT_HOUSING_TEMP_FMT "%lu %0.2f"
-#define MQTT_TOPIC_PID_ENABLED_FMT "%s/pid_enabled"
-#define MQTT_PID_ENABLED_FMT "%u"
-#define MQTT_TOPIC_PID_SETTINGS_FMT "%s/pid_settings_Kp_Ki_Kd"
-#define MQTT_PID_SETTINGS_FMT "%g;%g;%g"
-#define MQTT_SET_MSG_PID_SETTINGS_FMT MQTT_PID_SETTINGS_FMT
-#define MQTT_SUBTOPIC_CFG_FMT "%s/config"
-#define MQTT_SUBTOPIC_GET_FMT "%s/get"
-#define MQTT_SUBTOPIC_SET_FMT "%s/set"
-#define MQTT_TOPIC_DUTY_CYCLE_FMT "%s/duty_cycle"
-#define MQTT_DUTY_CYCLE_FMT "%g"
-#define MQTT_GET_MSG_DUTY_CYCLE "duty_cycle"
-#define MQTT_GET_MSG_PROGRAMS "programs"
-#define MQTT_TOPIC_TARGET_TEMP_FMT "%s/target_temperature_C"
-#define MQTT_TARGET_TEMP_FMT "%lu"
-#define MQTT_ON_CONN_MSG "connected"
 #include "mqtt_cred.h"
 EspMQTTClient *mqtt_cli = NULL;
 
@@ -60,14 +38,9 @@ unsigned long pwm_window_start_time = 0;
 
 // PID object
 PID_v2 *pid = NULL;
-#define PID_KP 10.0
-#define PID_KI 0.2
-#define PID_KD 0.1
 boolean pid_enabled = false;
 uint16_t target_temperature_C = 0;
 unsigned long pid_output = 0;
-#define MIN_TARGET_TEMP 0
-#define MAX_TARGET_TEMP 1100
 
 // LCD
 #define LCD_CLK 1
@@ -82,47 +55,19 @@ const uint8_t LCD_BOOT[] = {
 uint8_t lcd_val = 0;
 bool dots[4] = { false, false, false, false };
 
-// prototypes
-void config_set_thermo_update_int_ms(uint16_t thermo_update_int_ms);
-void config_set_pwm_update_int_ms(uint16_t pwm_update_int_ms);
-void config_set_mqtt_update_int_ms(uint16_t mqtt_update_int_ms);
-void config_set_pid_init_Ki(double Ki);
-void config_set_pid_init_Kd(double Kd);
-void config_set_pid_init_Kp(double Kp);
-void onConnectionEstablished(void);
-void onConfigMessageReceived(const String &message);
-void onStateSetMessageReceived(const String &message);
-void onStateSetMessageReceived(const String &message);
-void lcd_update(uint16_t val, bool bold, bool colon);
-
-// configuration
-#define PRFS_PID_KI "initial_Ki"
-#define PRFS_PID_KP "initial_Kp"
-#define PRFS_PID_KD "initial_Kd"
-#define PRFS_THRM_UPD_INT_MS "thrm_upd_int_ms"
-#define PRFS_PWM_UPD_INT_MS "pwm_upd_int_ms"
-#define PRFS_MQTT_UPD_INT_MS "mqtt_upd_int_ms"
+// Preferences interface
 Preferences preferences;
-#define MAX_CFG_STR 32
-#define MAC_FMT_STR "%02X%02X%02X%02X%02X%02X"  
-#define MIN_THERMO_UPDATE_MS 250
-#define MAX_THERMO_UPDATE_MS 5000
-#define MIN_PWM_UPDATE_MS 5000
-#define MAX_PWM_UPDATE_MS 20000
-#define MIN_MQTT_UPDATE_MS 250
-#define MAX_MQTT_UPDATE_MS 15000
 struct Config {
-  char mac[MAX_CFG_STR] = "c0:ff:ee:ca:fe:42";
+  char mac[MAX_CFG_STR] = MAC_DEFAULT;
   char topic[MAX_CFG_STR] = "";
   uint16_t thermo_update_int_ms = 250;
   uint16_t pwm_update_int_ms = 5000;
   uint16_t mqtt_update_int_ms = 1000;
   uint8_t min_loop_ms = 5;
-  double Kp = 0, Ki = 0, Kd = 0;
+  double Kp = PID_KP, Ki = PID_KI, Kd = PID_KD;
 } config;
-#define PRFS_CONFIG_JSON "config.json"
 
-void configLoad(String jsonString) {
+void configLoad(const String &jsonString) {
   JsonDocument doc;
 
   DeserializationError error = deserializeJson(doc, jsonString);
@@ -132,12 +77,12 @@ void configLoad(String jsonString) {
     Serial.println(error.f_str());
     return;
   }
-  config_set_thermo_update_int_ms(doc[PRFS_THRM_UPD_INT_MS] | MIN_THERMO_UPDATE_MS);
-  config_set_pwm_update_int_ms(doc[PRFS_PWM_UPD_INT_MS] | MIN_PWM_UPDATE_MS);
-  config_set_mqtt_update_int_ms(doc[PRFS_MQTT_UPD_INT_MS] | MIN_MQTT_UPDATE_MS);
-  config_set_pid_init_Ki(doc[PRFS_PID_KP] | PID_KP);
-  config_set_pid_init_Kd(doc[PRFS_PID_KI] | PID_KI);
-  config_set_pid_init_Kp(doc[PRFS_PID_KD] | PID_KD);
+  config_set_thermo_update_int_ms(doc[PRFS_THRM_UPD_INT_MS] | config.thermo_update_int_ms);
+  config_set_pwm_update_int_ms(doc[PRFS_PWM_UPD_INT_MS] | config.pwm_update_int_ms);
+  config_set_mqtt_update_int_ms(doc[PRFS_MQTT_UPD_INT_MS] | config.mqtt_update_int_ms);
+  config_set_pid_init_Kp(doc[PRFS_PID_KP] | config.Kp);
+  config_set_pid_init_Ki(doc[PRFS_PID_KI] | config.Ki);
+  config_set_pid_init_Kp(doc[PRFS_PID_KD] | config.Kd);
 }
 
 String configSerialize(void) {
@@ -269,40 +214,32 @@ void lcd_update(uint16_t val, bool bold, bool colon) {
 
 // state or preallocated variables for loop
 unsigned long last_time = 0, now, delta_t;
-#define MAX_BUF 256
 char buf1[MAX_BUF], buf2[MAX_BUF];
 
-void mqtt_publish_temps(void) {
-    snprintf(buf1, MAX_BUF, MQTT_TOPIC_KILN_FMT, config.topic);
-    snprintf(buf2, MAX_BUF, MQTT_KILN_TEMP_FMT, kiln_thermo->lastTime(), kiln_thermo->getTemperatureC());
-    mqtt_cli->publish(buf1, buf2);
-    snprintf(buf1, MAX_BUF, MQTT_TOPIC_HOUSING_FMT, config.topic);
-    snprintf(buf2, MAX_BUF, MQTT_HOUSING_TEMP_FMT, housing_thermo->lastTime(), housing_thermo->getTemperatureC());
-    mqtt_cli->publish(buf1, buf2);
-}
-void mqtt_publish_pid_enabled(void) {
-    snprintf(buf1, MAX_BUF, MQTT_TOPIC_PID_ENABLED_FMT, config.topic);
-    snprintf(buf2, MAX_BUF, MQTT_PID_ENABLED_FMT, pid_enabled);
-    mqtt_cli->publish(buf1, buf2);
-}
-void mqtt_publish_pid_settings(void) {
-    double Kp = pid->GetKp(), Ki = pid->GetKi(), Kd = pid->GetKd();
+void mqtt_publish_state(bool active=false, bool pid_current=false) {
+  JsonDocument doc;
+  String jsonString;
 
-    snprintf(buf1, MAX_BUF, MQTT_TOPIC_PID_SETTINGS_FMT, config.topic);
-    snprintf(buf2, MAX_BUF, MQTT_PID_SETTINGS_FMT, Kp, Ki, Kd);
-    mqtt_cli->publish(buf1, buf2);
+  doc["kiln"]["time_ms"] = kiln_thermo->lastTime();
+  doc["kiln"]["temperature_C"] = kiln_thermo->getTemperatureC();
+  doc["housing"]["time_ms"] = kiln_thermo->lastTime();
+  doc["housing"]["temperature_C"] = kiln_thermo->getTemperatureC();
+  if (pid_enabled || active) {
+    doc["pid_enabled"] = pid_enabled;
+    doc["target_temperature_C"] = target_temperature_C;
+    doc["duty_cycle"] = 100. * (double)pid_output / (double)config.pwm_update_int_ms;
+  }
+  if (pid_enabled || pid_current) {
+    double Kp = pid->GetKp(), Ki = pid->GetKi(), Kd = pid->GetKd();
+    doc["pid"]["Kp"] = Kp;
+    doc["pid"]["Ki"] = Ki;
+    doc["pid"]["Kd"] = Kd;
+  }
+  serializeJson(doc, jsonString);
+  snprintf(buf1, MAX_BUF, MQTT_TOPIC_FMT, config.topic, MQTT_TOPIC_STATE);
+  mqtt_cli->publish(buf1, jsonString);
 }
-void mqtt_publish_duty_cycle(void) {
-    double duty_cycle = 100. * (double)pid_output / (double)config.pwm_update_int_ms;
-    snprintf(buf1, MAX_BUF, MQTT_TOPIC_DUTY_CYCLE_FMT, config.topic);
-    snprintf(buf2, MAX_BUF, MQTT_DUTY_CYCLE_FMT, duty_cycle);
-    mqtt_cli->publish(buf1, buf2);
-}
-void mqtt_publish_target_temp(void) {
-    snprintf(buf1, MAX_BUF, MQTT_TOPIC_TARGET_TEMP_FMT, config.topic);
-    snprintf(buf2, MAX_BUF, MQTT_TARGET_TEMP_FMT, target_temperature_C);
-    mqtt_cli->publish(buf1, buf2);
-}
+
 void mqtt_publish_programs(void) {
 
 }
@@ -353,7 +290,7 @@ void loop() {
   delta_t = now - last_time;
   // if we have waited long enough, update the thermos
   if (delta_t >= config.mqtt_update_int_ms) {
-    mqtt_publish_temps();
+    mqtt_publish_state();
     last_time = millis();
   }
   // do a minimal delay for the PWM and other service loops
@@ -430,145 +367,54 @@ void config_set_pid_init_Kd(double Kd) {
 
 
 // handle mqtt config messages
-#define MAX_MSG_BUF 256
 void onConfigMessageReceived(const String &message) {
-  bool config_updated = false;
-  char msg[MAX_MSG_BUF];
-  message.toCharArray(msg, MAX_MSG_BUF);
-
-  // the format should be key=value so if no = found, bad msg
-  char *eqptr = strchr(msg, '=');
-  if (eqptr) {
-    char *valptr = eqptr+1;
-    *eqptr = NULL;
-    if (strcmp(msg, PRFS_THRM_UPD_INT_MS) == 0) {
-      unsigned long val = strtoul(valptr, NULL, 0);
-      config_set_thermo_update_int_ms(val);
-      config_updated = true;
-    } else if (strcmp(msg, PRFS_PWM_UPD_INT_MS) == 0) {
-      unsigned long val = strtoul(valptr, NULL, 0);
-      config_set_pwm_update_int_ms(val);
-      config_updated = true;
-    } else if (strcmp(msg, PRFS_MQTT_UPD_INT_MS) == 0) {
-      unsigned long val = strtoul(valptr, NULL, 0);
-      config_set_mqtt_update_int_ms(val);
-      config_updated = true;
-    } else if (strcmp(msg, PRFS_PID_KP) == 0) {
-      double val = strtod(valptr, NULL);
-      config_set_pid_init_Kp(val);
-      config_updated = true;
-    } else if (strcmp(msg, PRFS_PID_KI) == 0) {
-      double val = strtod(valptr, NULL);
-      config_set_pid_init_Ki(val);
-      config_updated = true;
-    } else if (strcmp(msg, PRFS_PID_KD) == 0) {
-      double val = strtod(valptr, NULL);
-      config_set_pid_init_Kd(val);
-      config_updated = true;
-    }
-  }
-
-  if (config_updated) {
-    configUpdatePrefs();
-    Serial.println("system configuration updated, restarting...");
-    delay(1000);
-    esp_restart();
-  }
+  Serial.print("processing config update in json: ");
+  Serial.println(message);
+  configLoad(message);
 }
 
-#define MQTT_GET_MSG_TEMP "temperature_C"
-#define MQTT_SET_MSG_TARGET_TEMP "target_temperature_C"
-#define MQTT_GET_MSG_TARGET_TEMP MQTT_SET_MSG_TARGET_TEMP
-#define MQTT_SET_MSG_PID_ENABLED "pid_enabled"
-#define MQTT_GET_MSG_PID_ENABLED MQTT_SET_MSG_PID_ENABLED
-#define MQTT_SET_MSG_PID_SETTINGS "pid_settings_Kp_Ki_Kd"
-#define MQTT_GET_MSG_PID_SETTINGS MQTT_SET_MSG_PID_SETTINGS
 // handle mqtt state messages
 void onSetStateMessageReceived(const String &message) {
   char msg[MAX_MSG_BUF];
   message.toCharArray(msg, MAX_MSG_BUF);
 
   // the format should be key=value so if no = found, bad msg
-  char *eqptr = strchr(msg, '=');
-  if (eqptr) {
-    char *valptr = eqptr+1;
-    *eqptr = NULL;
-    if (strcmp(msg, MQTT_SET_MSG_TARGET_TEMP) == 0) {
-      unsigned long val = strtoul(valptr, NULL, 0);
-      if (val < MIN_TARGET_TEMP) {
-        val = MIN_TARGET_TEMP;
-      } else if (val > MAX_TARGET_TEMP) {
-        val = MAX_TARGET_TEMP;
-      }
-      Serial.print("target temperature set via mqtt to (C) ");
-      Serial.println(val);
-      target_temperature_C = val;
-      if (pid_enabled)
-        pid->Setpoint(target_temperature_C);
-      mqtt_publish_target_temp();
-    } else if (strcmp(msg, MQTT_SET_MSG_PID_ENABLED) == 0) {
-      bool val = strtoul(valptr, NULL, 0);
-      Serial.print("pid enabled is set to ");
-      Serial.println(val);
-      if (!pid_enabled && val) {
+      //if (pid_enabled)
+      //  pid->Setpoint(target_temperature_C);
+      //mqtt_publish_target_temp();
+      
+  //if (!pid_enabled && val) {
         // turning on
-        Serial.print("pid turning on with target temperature of (C) ");
-	Serial.println(target_temperature_C);
-        pid->Start(kiln_thermo->getTemperatureC(), 0, target_temperature_C);
-      } else if (pid_enabled && !val) {
+        //pid->Start(kiln_thermo->getTemperatureC(), 0, target_temperature_C);
+      //} else if (pid_enabled && !val) {
         // turning off
-        Serial.println("pid turning off");
-        ssr_off();
-      }
-      pid_enabled=val;
-      mqtt_publish_pid_enabled();
-    } else if (strcmp(msg, MQTT_SET_MSG_PID_SETTINGS) == 0) {
-      double Kp, Ki, Kd;
-      uint8_t parsed = sscanf(valptr, MQTT_SET_MSG_PID_SETTINGS_FMT, &Kp, &Ki, &Kd);
-      if ((Kp >= 0 & Ki >= 0 && Kd >= 0) && parsed == 3) {
-	Serial.print("updating instaneous tunings from mqtt Kp = ");
-	Serial.print(Kp);
-	Serial.print(" Ki = ");
-	Serial.print(Ki);
-	Serial.print(" Kd = ");
-	Serial.println(Kd);
-	if (pid)
-	  pid->SetTunings(Kp, Ki, Kd);
-      }
-    }
-  }
+        //ssr_off();
+      //pid_enabled=val;\
+
+      //double Kp, Ki, Kd;
+      //uint8_t parsed = sscanf(valptr, MQTT_SET_MSG_PID_SETTINGS_FMT, &Kp, &Ki, &Kd);
+      //if ((Kp >= 0 & Ki >= 0 && Kd >= 0) && parsed == 3) {
+//	Serial.print("updating instaneous tunings from mqtt Kp = ");
+	//if (pid)
+	 // pid->SetTunings(Kp, Ki, Kd);
 }
 void onGetStateMessageReceived(const String &message) {
   char msg[MAX_MSG_BUF];
   message.toCharArray(msg, MAX_MSG_BUF);
 
-  // the format should be key=value so if no = found, bad msg
-  if (strcmp(msg, MQTT_GET_MSG_TEMP) == 0) {
-    mqtt_publish_temps();
-  } else if (strcmp(msg, MQTT_GET_MSG_TARGET_TEMP) == 0) {
-    mqtt_publish_target_temp();
-  } else if (strcmp(msg, MQTT_GET_MSG_PID_ENABLED) == 0) {
-    mqtt_publish_pid_enabled();
-  } else if (strcmp(msg, MQTT_GET_MSG_PID_SETTINGS) == 0) {
-    mqtt_publish_pid_settings();
-  } else if (strcmp(msg, MQTT_GET_MSG_DUTY_CYCLE) == 0) {
-    mqtt_publish_duty_cycle();
-  } else if (strcmp(msg, MQTT_GET_MSG_PROGRAMS) == 0) {
-    mqtt_publish_programs();
-  }
 }
 
 // when the connection to the mqtt has completed
 void onConnectionEstablished(void) {
-  char topic[MQTT_MAX_TOPIC_STR];
+  char topic[MAX_BUF];
 
-  snprintf(topic, MQTT_MAX_TOPIC_STR, MQTT_SUBTOPIC_CFG_FMT, config.topic);
+  snprintf(topic, MAX_BUF, MQTT_TOPIC_FMT, config.topic, MQTT_TOPIC_CONFIG);
   mqtt_cli->subscribe(topic, onConfigMessageReceived);
 
-  snprintf(topic, MQTT_MAX_TOPIC_STR, MQTT_SUBTOPIC_GET_FMT, config.topic);
+  snprintf(topic, MAX_BUF, MQTT_TOPIC_FMT, config.topic, MQTT_TOPIC_GET);
   mqtt_cli->subscribe(topic, onGetStateMessageReceived);
-  snprintf(topic, MQTT_MAX_TOPIC_STR, MQTT_SUBTOPIC_SET_FMT, config.topic);
+  snprintf(topic, MAX_BUF, MQTT_TOPIC_FMT, config.topic, MQTT_TOPIC_SET);
   mqtt_cli->subscribe(topic, onSetStateMessageReceived);
 
-  mqtt_cli->publish(config.topic, MQTT_ON_CONN_MSG);
+  mqtt_publish_state();
 }
